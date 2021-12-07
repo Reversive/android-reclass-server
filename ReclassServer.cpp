b549929ab409ec68a7f777f2a3e9f4d33e611110
Reversive <matiaspavan@gmail.com>
2021-12-07 12:27:35 -0300

    Added cleanup for server termination

--- ReclassServer.cpp
+++ ReclassServer.cpp
@@ -12,31 +12,44 @@ void sigHandler(int signo) {
 }
 
 int main(int argc, char *argv[]) {
+    int status = 0;
     long port = strtol(argv[1], NULL, 10);
     log(INFO, "%s", "Remember to 'adb forward tcp:<port> tcp:<port>' before running this program.");
     log(INFO, "Starting server on port %ld...", port);
     signal(SIGINT, sigHandler);
     server = new PassiveSocket(port);
+    if(server == NULL) {
+        log(ERROR, "%s", "Could not create server socket.");
+        status = -1;
+        goto cleanup;
+    }
 
     if(server->bind() == false) {
         log(ERROR, "%s", "Bind failed");
-        exit(1);
+        status = -2;
+        goto cleanup;
     }
 
     if(server->listen() == false) {
         log(ERROR, "%s", "Listen failed");
-        exit(1);
+        status = -3;
+        goto cleanup;
     }
 
     log(INFO, "TCP server started on port %ld", port);
 
     if(server->accept() == false) {
         log(ERROR, "%s", "Accept failed");
-        exit(1);
+        status = -4;
+        goto cleanup;
     }
 
     log(INFO, "%s", "Client connected");
-    
-    server->close();
-    return 0;
+
+cleanup:
+
+    if(server != NULL) {
+        delete server;
+    }
+    return status;
 }
