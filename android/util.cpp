07eecc18a8b08ed7d5df3a9855d375ab357e6f65
Reversive <matiaspavan@gmail.com>
2025-12-24 10:09:54 -0300

    fix: add RAII wrapper for DIR* and use named constants

--- android/util.cpp
+++ android/util.cpp
@@ -1,26 +1,42 @@
 #include <util.hpp>
 #include <cstdio>
 #include <cstring>
+#include <memory>
+
+namespace
+{
+    constexpr size_t MAX_PATH_LEN = 64;
+    constexpr size_t MAX_PROCESS_NAME_LEN = 64;
+
+    struct DirDeleter
+    {
+        void operator()(DIR* dir) const noexcept
+        {
+            if (dir) closedir(dir);
+        }
+    };
+    using DirPtr = std::unique_ptr<DIR, DirDeleter>;
+}
 
 std::vector<process_info> android::util::get_processes()
 {
     std::vector<process_info> processes;
-    DIR *dir = opendir("/proc");
-    if (dir == nullptr)
+    DirPtr dir(opendir("/proc"));
+    if (!dir)
     {
         return processes;
     }
     dirent *entry;
-    while ((entry = readdir(dir)) != nullptr)
+    while ((entry = readdir(dir.get())) != nullptr)
     {
         int process_id = -1;
         if (sscanf(entry->d_name, "%d", &process_id) != 1)
         {
             continue;
         }
-        char cmdline_path[64];
+        char cmdline_path[MAX_PATH_LEN];
         snprintf(cmdline_path, sizeof(cmdline_path), "/proc/%d/cmdline", process_id);
-        char process_name[64];
+        char process_name[MAX_PROCESS_NAME_LEN];
         int fd = open(cmdline_path, O_RDONLY | O_CLOEXEC);
         if (fd < 0)
         {
@@ -39,6 +55,5 @@ std::vector<process_info> android::util::get_processes()
         }
         processes.emplace_back(process_info(process_id, process_name));
     }
-    closedir(dir);
     return processes;
 }
